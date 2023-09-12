a873d4b30b6734b2df7f15fcbd7ad326b2e9ccc1
damitymo <tymoszukdamiandejesus@gmail.com>
2023-09-12 18:45:24 -0300

    modificado

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,12 +5,17 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   return nombre[0].toUpperCase()+nombre.slice(1);
+
+   
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   cb();
+
 }
 
 function operacionMatematica(num1, num2, cb) {
@@ -18,6 +23,8 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   return cb(num1,num2);
+
 }
 
 function sumarArray(arrayOfNumbers, cb) {
