e0b79d3144d8b712c76fb5211b781784a0e13ba1
damitymo <tymoszukdamiandejesus@gmail.com>
2023-09-12 21:40:08 -0300

    finalizado el M08

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -33,13 +33,23 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
-}
+   var sumaN=0;
+   for (let i = 0; i<arrayOfNumbers.length;i++){
+         sumaN += arrayOfNumbers[i];
+       }
+       cb(sumaN);
+   }      
+   
+
 
 function forEach(array, cb) {
    // Recibes un arreglo y un callback.
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+   array.forEach(element => {
+      cb(element)      
+   });
 }
 
 function map(array, cb) {
@@ -47,12 +57,21 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   
+   var arrayN=array.map( (num) => {
+      return cb(num)
+   } );
+  return arrayN;
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   var arrayA=[];
+   arrayA=arrayOfStrings.filter((letra)=> letra.charAt(0)==="a")
+   return arrayA;
+
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
