83ea1ebe37fe276b0a0e87a67f3b0f7c6ad82dc9
Damian de Jesus Tymoszuk <tymoszukdamiandejesus@gmail.com>
2025-07-01 12:56:01 -0300

    Actualizar homework.js

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -46,9 +46,7 @@ function esDiezOCinco(num) {
    // Retornar true si "num" es 10 o 5.
    // De lo contrario, retornar false.
    // Tu código:
-   if (num===5||num===10){return true;}
-   else{return false;}
-}
+   
 
 function estaEnRango(num) {
    // Retornar true si "num" es menor que 50 y mayor que 20.
@@ -81,15 +79,7 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
-   if (num1>num2 && num1>num3 && num1>0){return "Numero 1 es mayor y positivo";}
-   else if(num1<0 ||num2<0 ||num3<0){return "Hay negativos";}
-   else if (num3>num2 && num3 >num1){
-      num3++;
-      return num3;
-   }
-   else if(num1===0&&num2===0&&num3===0){return "Error";}
-   else{return false;}
-}
+   
 
 function esPrimo(num) {
    // Retornar true si "num" es primo.
@@ -98,45 +88,26 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
-   var cont=0;
-   for (var i=1; i<=num; i++){
-      if (num%i===0){
-         cont++;
-      }
-   }
-   if (cont==2){return true;}
-   else{return false;}
-}
+  
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
    // Tu código:
-   if (valor===true){return "Soy verdadero";}
-   else{ return "Soy falso";}
-}
+  
 
 function tieneTresDigitos(num) {
    // Si el número recibido tiene tres dígitos retornar true.
    // Caso contrario, retornar false.
    // Tu código:
-   if (num>99&&num<1000){return true;}
-   else{return false;}
-}
+   
 
 function doWhile(num) {
    // Implementar una función que aumente el valor recibido en 5 hasta un límite de 8 veces.
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
-   var cont=0;
-   do{
-      num+=5;
-      cont++;
-   }
-   while(cont<8);
-   return num;
-}
+   
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
