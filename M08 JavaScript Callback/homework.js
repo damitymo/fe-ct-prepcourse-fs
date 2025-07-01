3ce7dda53e42d0a347951a1eb6661291a16fcb40
Damian de Jesus Tymoszuk <tymoszukdamiandejesus@gmail.com>
2025-07-01 13:17:03 -0300

    Actualizar homework.js

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,8 +5,7 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
-   return nombre[0].toUpperCase()+nombre.slice(1);
-
+   
    
 }
 
@@ -14,7 +13,7 @@ function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
-   cb();
+   
 
 }
 
@@ -23,8 +22,7 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
-   return cb(num1,num2);
-
+   
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -33,11 +31,7 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
-   var sumaN=0;
-   for (let i = 0; i<arrayOfNumbers.length;i++){
-         sumaN += arrayOfNumbers[i];
-       }
-       cb(sumaN);
+   
    }      
    
 
@@ -47,9 +41,7 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
-   array.forEach(element => {
-      cb(element)      
-   });
+   
 }
 
 function map(array, cb) {
@@ -58,20 +50,14 @@ function map(array, cb) {
    // Retorna el nuevo arreglo.
    // Tu código:
    
-   var arrayN=array.map( (num) => {
-      return cb(num)
-   } );
-  return arrayN;
+   
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
-   var arrayA=[];
-   arrayA=arrayOfStrings.filter((letra)=> letra.charAt(0)==="a")
-   return arrayA;
-
+   
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
